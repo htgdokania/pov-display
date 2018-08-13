33c4f82fdd777501838dbb1d966b40564cf61fc3
HARSH DOKANIA <34301506+htgdokania@users.noreply.github.com>
2018-08-14 01:33:28 +0530

    Add files via upload

---
+++ pov_bluetooth/pov_bluetooth.ino
@@ -0,0 +1,228 @@
+int NUMBER9[]={1,1,1,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,1,1,1,1,1,1,1};
+ int NUMBER8[]={0,1,1,0,1,1,1,0,
+                1,0,0,1,0,0,0,1,
+                1,0,0,1,0,0,0,1,
+                1,0,0,1,0,0,0,1,
+                0,1,1,0,1,1,1,0};
+ int NUMBER7[]={1,0,0,0,0,0,0,0,
+                1,0,0,0,1,0,0,0,
+                1,0,0,0,1,0,0,0,
+                1,0,0,1,1,1,1,1,
+                1,1,1,0,1,0,0,0};
+ int NUMBER6[]={1,1,1,1,1,1,1,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,1,1,1};
+ int NUMBER5[]={1,1,1,1,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,1,1,1};
+ int NUMBER2[]= {1,0,0,0,0,0,1,1,
+                 1,0,0,0,0,1,0,1,
+                 1,0,0,0,1,0,0,1,
+                 1,0,0,1,0,0,0,1,
+                 0,1,1,0,0,0,0,1};
+ int NUMBER1[]= {0,0,1,0,0,0,0,0,
+                 0,1,0,0,0,0,0,0,
+                 1,1,1,1,1,1,1,1,
+                 0,0,0,0,0,0,0,0,
+                 0,0,0,0,0,0,0,0};
+ int NUMBER0[]= {1,1,1,1,1,1,1,1,
+                 1,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,1,
+                 1,1,1,1,1,1,1,1};
+
+ int _[] = {0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0};
+ int A[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,1,1,1,1,1,1,1};
+ int B[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            0,1,1,0,1,1,1,0};
+ int C[] = {0,0,1,1,1,1,0,0,
+            0,1,0,0,0,0,1,0,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1};
+ int D[] = {1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            0,1,0,0,0,0,1,0,
+            0,0,1,1,1,1,0,0};
+ int E[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1};
+ int F[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0};
+ int G[] = {0,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,1,0,0,1,
+            1,0,0,0,1,0,0,1,
+            1,0,0,0,1,1,1,0};
+ int H[] = {1,1,1,1,1,1,1,1,
+            0,0,0,0,1,0,0,0,
+            0,0,0,0,1,0,0,0,
+            0,0,0,0,1,0,0,0,
+            1,1,1,1,1,1,1,1};
+ int I[] = {1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1};
+ int J[] = {0,0,0,0,0,1,1,0,
+            0,0,0,0,1,0,0,1,
+            0,0,0,0,0,0,0,1,
+            0,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,0};
+ int K[] = {1,1,1,1,1,1,1,1, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 0,1,0,0,0,0,1,0, 1,0,0,0,0,0,0,1};
+ int L[] = {1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1};
+ int M[] = {1,1,1,1,1,1,1,1, 0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,1,0,0,0,0,0,0, 1,1,1,1,1,1,1,1};
+ int N[] = {1,1,1,1,1,1,1,1, 0,0,1,0,0,0,0,0, 0,0,0,1,1,0,0,0, 0,0,0,0,0,1,0,0, 1,1,1,1,1,1,1,1};
+ int O[] = {0,1,1,1,1,1,1,1,0,
+            1,0,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,0,1,
+            0,1,1,1,1,1,1,1,0};
+ int P[] = {1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 0,1,1,0,0,0,0,0};
+ int Q[] = {0,1,1,1,1,1,1,0, 1,0,0,0,0,0,0,1, 1,0,0,0,0,1,0,1, 0,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1};
+ int R[] = {1,1,1,1,1,1,1,1, 1,0,0,1,1,0,0,0, 1,0,0,1,0,1,0,0, 1,0,0,1,0,0,1,0, 0,1,1,0,0,0,0,1};
+ int S[] = {0,1,1,1,0,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,0};
+ int T[] = {1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0};
+ int U[] = {1,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,0};
+ int V[] = {1,1,1,1,1,1,0,0, 0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,0,0};
+ int W[] = {1,1,1,1,1,1,1,1, 0,0,0,0,0,0,1,0, 0,0,0,0,0,1,0,0, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,1,1};
+ int X[] = {1,1,0,0,0,0,1,1, 0,0,1,0,0,1,0,0, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 1,1,0,0,0,0,1,1};
+ int Y[] = {1,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,0,0,1,1,1,1,1, 0,0,1,0,0,0,0,0, 1,1,0,0,0,0,0,0};
+ int Z[] = {1,0,0,0,0,1,1,1, 1,0,0,0,1,0,0,1, 1,0,0,1,0,0,0,1, 1,0,1,0,0,0,0,1, 1,1,0,0,0,0,0,1};
+ int* alpha[]= {A,B,C,D,E,F,G,H,I,J,K,L,M,N};//,T,U,V,W,X,Y,Z};
+ int letterSpace=2;
+ int dotTime=500;
+ int start_gap=20;
+ int charac[50];
+void setup()
+{
+ Serial.begin(9600);
+// setting the ports of the leds to OUTPUT
+ for( int i = 2; i<10 ;i++ )
+ { pinMode(i, OUTPUT);
+ }
+ 
+ //input ir digital value
+ pinMode(11,OUTPUT);//ir led or +5v
+ pinMode(12,INPUT);//data pin
+ pinMode(A7,INPUT);//ground pin
+ digitalWrite(11,1);//set ir led to +5v 
+ digitalWrite(A7,0);//set ir led to +5v 
+}
+
+
+ void printLetter(int letter[])
+ {
+ int y;
+// printing the first y row of the letter
+ for (y=0; y<8; y++)
+ {
+ digitalWrite(y+2, letter[y]);
+ }
+ delayMicroseconds(dotTime);
+// printing the second y row of the letter
+ for (y=0; y<8; y++)
+ {
+ digitalWrite(y+2, letter[y+8]);
+ }
+ delayMicroseconds(dotTime);
+// printing the third y row of the letter
+ for (y=0; y<8; y++)
+ {
+ digitalWrite(y+2, letter[y+16]);
+ }
+ delayMicroseconds(dotTime);
+ for(y = 0; y<8; y++) {
+ digitalWrite(y+2, letter[y+24]);
+ }
+ delayMicroseconds(dotTime);
+for(y = 0; y<8; y++) {
+ digitalWrite(y+2, letter[y+32]);
+ }
+ delayMicroseconds(dotTime);
+ // printing the sspace between the letters
+ for (y=0; y<8; y++)
+ {
+ digitalWrite(y+2, 0);
+ }
+ delay(letterSpace);
+ }
+
+ 
+ void loop()
+{
+  Serial.println(digitalRead(12));
+  
+  if(Serial.available())
+  {
+       int  ch=Serial.read();
+       Serial.println(ch);
+       
+       switch (ch)
+        {
+            case'A':case 'a' : printLetter (A); ; break;
+            case'B':case 'b' : printLetter (B); ; break;
+            case'C':case 'c' : printLetter (C); ; break;
+            case'D':case 'd' : printLetter (D); ; break;
+            case'E':case 'e' : printLetter (E); ; break;
+            case'F':case 'f' : printLetter (F); ; break;
+            case'G':case 'g' : printLetter (G); ; break;
+            case'H':case 'h' : printLetter (H); ; break;
+            case'I':case 'i' : printLetter (I); ; break;
+            case'J':case 'j' : printLetter (J); ; break;
+            case'K':case 'k' : printLetter (K); ; break;
+            case'L':case 'l' : printLetter (L); ; break;
+            case'M':case 'm' : printLetter (M) ; break;
+            case'N':case 'n' : printLetter (N); ; break;
+            case'O':case 'o' : printLetter (O); ; break;
+            case'P':case 'p' : printLetter (P); ; break;
+            case'Q':case 'q' : printLetter (Q); ; break;
+            case'R':case 'r' : printLetter (R); ; break;
+            case'S':case 's' : printLetter (S); ; break;
+            case'T':case 't' : printLetter (T); ; break;
+            case'U':case 'u' : printLetter (U); ; break;
+            case'V':case 'v' : printLetter (V); ; break;
+            case'W':case 'w' : printLetter (W); ; break;
+            case'X':case 'x' : printLetter (X); ; break;
+            case'Y':case 'y' : printLetter (Y); ; break;
+            case'Z':case 'z' : printLetter (Z); ; break;
+        }
+    }
+  }
+  
+  else
+  if(digitalRead(12)==1)
+  {
+    //write whatever you would like to print inside
+    delay(start_gap);
+    printLetter (C);
+    printLetter (C);
+    printLetter (A); 
+   }
+   
+}
---
+++ pov_for_300rpm/pov_for_300rpm.ino
@@ -0,0 +1,244 @@
+int NUMBER9[]={1,1,1,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,0,0,1,0,0,0,1,
+               1,1,1,1,1,1,1,1};
+ int NUMBER8[]={0,1,1,0,1,1,1,0,
+                1,0,0,1,0,0,0,1,
+                1,0,0,1,0,0,0,1,
+                1,0,0,1,0,0,0,1,
+                0,1,1,0,1,1,1,0};
+ int NUMBER7[]={1,0,0,0,0,0,0,0,
+                1,0,0,0,1,0,0,0,
+                1,0,0,0,1,0,0,0,
+                1,0,0,1,1,1,1,1,
+                1,1,1,0,1,0,0,0};
+ int NUMBER6[]={1,1,1,1,1,1,1,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,1,1,1};
+ int NUMBER5[]={1,1,1,1,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,0,0,1,
+                1,0,0,0,1,1,1,1};
+ int NUMBER2[]= {1,0,0,0,0,0,1,1,
+                 1,0,0,0,0,1,0,1,
+                 1,0,0,0,1,0,0,1,
+                 1,0,0,1,0,0,0,1,
+                 0,1,1,0,0,0,0,1};
+ int NUMBER1[]= {0,0,1,0,0,0,0,0,
+                 0,1,0,0,0,0,0,0,
+                 1,1,1,1,1,1,1,1,
+                 0,0,0,0,0,0,0,0,
+                 0,0,0,0,0,0,0,0};
+ int NUMBER0[]= {1,1,1,1,1,1,1,1,
+                 1,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,1,
+                 1,1,1,1,1,1,1,1};
+
+ int _[] = {0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,0};
+ int A[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,1,1,1,1,1,1,1};
+ int B[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            0,1,1,0,1,1,1,0};
+ int C[] = {0,0,1,1,1,1,0,0,
+            0,1,0,0,0,0,1,0,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1};
+ int D[] = {1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            0,1,0,0,0,0,1,0,
+            0,0,1,1,1,1,0,0};
+ int E[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1,
+            1,0,0,1,0,0,0,1};
+ int F[] = {1,1,1,1,1,1,1,1,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0,
+            1,0,0,1,0,0,0,0};
+ int G[] = {0,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,1,0,0,1,
+            1,0,0,0,1,0,0,1,
+            1,0,0,0,1,1,1,0};
+ int H[] = {1,1,1,1,1,1,1,1,
+            0,0,0,0,1,0,0,0,
+            0,0,0,0,1,0,0,0,
+            0,0,0,0,1,0,0,0,
+            1,1,1,1,1,1,1,1};
+ int I[] = {1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1};
+ int J[] = {0,0,0,0,0,1,1,0,
+            0,0,0,0,1,0,0,1,
+            0,0,0,0,0,0,0,1,
+            0,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,0};
+ int K[] = {1,1,1,1,1,1,1,1, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 0,1,0,0,0,0,1,0, 1,0,0,0,0,0,0,1};
+ int L[] = {1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1};
+ int M[] = {1,1,1,1,1,1,1,1, 0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,1,0,0,0,0,0,0, 1,1,1,1,1,1,1,1};
+ int N[] = {1,1,1,1,1,1,1,1, 0,0,1,0,0,0,0,0, 0,0,0,1,1,0,0,0, 0,0,0,0,0,1,0,0, 1,1,1,1,1,1,1,1};
+ int O[] = {0,1,1,1,1,1,1,0,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,1,
+            0,1,1,1,1,1,1,0};
+ int P[] = {1,1,1,1,1,1,1,1, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 1,0,0,1,0,0,0,0, 0,1,1,0,0,0,0,0};
+ int Q[] = {0,1,1,1,1,1,1,0, 1,0,0,0,0,0,0,1, 1,0,0,0,0,1,0,1, 0,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1};
+ int R[] = {1,1,1,1,1,1,1,1, 1,0,0,1,1,0,0,0, 1,0,0,1,0,1,0,0, 1,0,0,1,0,0,1,0, 0,1,1,0,0,0,0,1};
+ int S[] = {0,1,1,1,0,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,0,0,1, 1,0,0,0,1,1,1,0};
+ int T[] = {1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,0, 1,0,0,0,0,0,0,0};
+ int U[] = {1,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,0};
+ int V[] = {1,1,1,1,1,1,0,0, 0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,0,0};
+ int W[] = {1,1,1,1,1,1,1,1, 0,0,0,0,0,0,1,0, 0,0,0,0,0,1,0,0, 0,0,0,0,0,0,1,0, 1,1,1,1,1,1,1,1};
+ int X[] = {1,1,0,0,0,0,1,1, 0,0,1,0,0,1,0,0, 0,0,0,1,1,0,0,0, 0,0,1,0,0,1,0,0, 1,1,0,0,0,0,1,1};
+ int Y[] = {1,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0, 0,0,0,1,1,1,1,1, 0,0,1,0,0,0,0,0, 1,1,0,0,0,0,0,0};
+ int Z[] = {1,0,0,0,0,1,1,1, 1,0,0,0,1,0,0,1, 1,0,0,1,0,0,0,1, 1,0,1,0,0,0,0,1, 1,1,0,0,0,0,0,1};
+ int* alpha[]= {A,B,C,D,E,F,G,H,I,J,K,L,M,N};//,T,U,V,W,X,Y,Z};
+ int letterSpace=2;
+ int dotTime=1;
+ int start_gap=90;
+ int a,b,c;
+ char wor[20];
+char temp[20];
+  
+void setup()
+{
+ Serial.begin(9600);
+// setting the ports of the leds to OUTPUT
+ for( int i = 2; i<10 ;i++ )
+ {
+  pinMode(i, OUTPUT);
+ }
+ 
+ //input ir digital value
+ pinMode(11,OUTPUT);//ir led or +5v
+ pinMode(12,INPUT);//data pin
+ pinMode(A7,INPUT);//ground pin
+ digitalWrite(11,1);//set ir led to +5v 
+ digitalWrite(A7,0);//set ir led to +5v 
+}
+
+
+ void printLetter(int letter[])
+ {
+  int y;
+  // printing the first y row of the letter
+  for (y=0; y<8; y++)
+  {
+  digitalWrite(y+2, letter[y]);
+  }
+  delay(dotTime);
+  // printing the second y row of the letter
+  for (y=0; y<8; y++)
+  {
+  digitalWrite(y+2, letter[y+8]);
+  }
+  delay(dotTime);
+  // printing the third y row of the letter
+  for (y=0; y<8; y++)
+  {
+  digitalWrite(y+2, letter[y+16]);
+  }
+  delay(dotTime);
+  for(y = 0; y<8; y++) {
+  digitalWrite(y+2, letter[y+24]);
+  }
+  delay(dotTime);
+  for(y = 0; y<8; y++) {
+  digitalWrite(y+2, letter[y+32]);
+  }
+  delay(dotTime);
+  // printing the sspace between the letters
+  for (y=0; y<8; y++)
+  {
+  digitalWrite(y+2, 0);
+  }
+  delay(letterSpace);
+ }
+ 
+void callLetter(int num)
+{
+  switch (num)
+  {
+    case 65: printLetter(A);break;
+    case 66: printLetter(B);break;
+    case 67: printLetter(C);break;
+    case 68: printLetter(D);break;
+    case 69: printLetter(E);break;
+    case 70: printLetter(F);break;
+    case 71: printLetter(G);break;
+    case 72: printLetter(H);break;
+    case 73: printLetter(I);break;
+    case 74: printLetter(J);break;
+    case 75: printLetter(K);break;
+    case 76: printLetter(L);break;
+    case 77: printLetter(M);break;
+    case 78: printLetter(N);break;
+    case 79: printLetter(O);break;
+    case 80: printLetter(P);break;
+    case 81: printLetter(Q);break;
+    case 82: printLetter(R);break;
+    case 83: printLetter(S);break;
+    case 84: printLetter(T);break;/*
+    case 85: printLetter(U);break;
+    case 86: printLetter(V);break;
+    case 87: printLetter(W);break;
+    case 88: printLetter(X);break;
+    case 89: printLetter(Y);break;
+    case 90: printLetter(Z);break;*/
+    default: printLetter(_);
+  }
+}
+ void word(char wor[],int len)
+ {
+  for(a=0;a<5000;a++)
+  {
+    
+  if(digitalRead(12)==1)
+    {    
+      //write whatever you would like to print inside
+      delay(start_gap);
+      b=0;
+      
+      while(b<len)
+      { 
+        c=wor[b];
+        callLetter(c);
+        b++;
+      }
+    }
+    else
+      continue;
+  }
+}
+ 
+ void loop()
+{
+  //Serial.println(digitalRead(12));
+  strcpy(temp,"CCA");
+  word(temp,3);
+  //delay(100);
+  strcpy(temp,"ROBOCELL");
+  word(temp,8);
+}
